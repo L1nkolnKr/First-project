3dd67e8c22a25cf3a0ade7aff4e112eccc6bfcf8
L1nkoln <mr.aleks111@gmail.com>
2020-12-22 02:16:16 +0200

    primary structure

---
+++ src/Components/Content.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Content= ()=>{
+    return <div className='content'>
+        <div>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkjIGjG0hLXWH59WpMEd_8zLjyy1VlM_Q7eA&usqp=CAU'></img>
+        </div>
+        <div>
+          ava+description
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            Post1
+          </div>
+          <div>
+            Post2
+          </div>
+        </div>
+    </div>
+}
+
+export default Content;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header= ()=>{
+    return <header className='header'>
+        <img src='https://image.freepik.com/free-vector/car-logo-template_9298-22.jpg'></img>
+
+    </header>
+}
+export default Header;
